2bc0a310c702c4a7ca4a638e1c4088d914f9a9ad
KR2423 <ckeerthi.r23@gmail.com>
2025-07-27 17:30:57 -0700

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,67 @@
+
+//DataTypes -  the type of data a variable can hold
+
+public class DataTypes {
+    public static void main(String[] args) {
+      //Boolean - 
+       boolean b1 = true;
+       boolean b2 = false; 
+       System.out.println("Dog lives on land " + b1);
+       System.out.println("Cat lives in water " + b2);
+
+       //Byte - 1 byte (-128 to 127)
+       byte B1 = 22;
+       byte B2 = -35;
+       System.out.println("byte number1 " + B1);
+       System.out.println("byte number2 " + B2);
+
+      //Short - 2 bytes (-32,768 to 32,767)
+      short S1 = 32456;
+      short S2 = -32768;
+      System.out.println("short number1 " + S1);
+      System.out.println("short number2 " + S2);
+
+      //Integer 
+      int I1 = 2345678;
+      int I2 = 768954399;
+      System.out.println("integer number1 " + I1);
+      System.out.println("integer number2 " + I2);
+
+      //Long - 8 bytes (add 'L' at the end)
+      long L1 = 98765432343456789L;
+      long L2 = 987654312345678989L;
+      System.out.println("long number1 " + L1);
+      System.out.println("long number2 " + L2);
+
+      //floating point - 4 bytes (add 'f' at the end)
+      float F1 = 3.456789999999F;
+      float F2 = 3.3422F;
+      System.out.println("float number1 " + F1);
+      System.out.println("float number2 " + F2);
+
+      //double - 8 bytes (default for decimals)
+      // recommended to use for big decimal numbers
+      double D1 = 3.45678987654567890876545;
+      double D2 = 9.87654345678987654323456789;
+      System.out.println("double number1 " + D1);
+      System.out.println("double number2 " + D2);
+
+      //char - used to store a single character value
+      char C1 = 'K';
+      char C2 = 'R';
+      System.out.println("char1 " + C1);
+      System.out.println("char2 " + C2);
+
+
+
+
+
+
+
+
+    }
+
+
+    
+    
+}

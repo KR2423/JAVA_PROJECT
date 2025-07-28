34d6758da0676d7b28e8c302d045957885920677
KR2423 <ckeerthi.r23@gmail.com>
2025-07-27 17:32:19 -0700

    Add files via upload

---
+++ Fruit.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Fruit {
+    String Myname;
+    int Mynumber;
+
+    public void F1(){
+        System.out.println("I am from Fruit class from " +Myname);
+    }
+    public void F2(){
+        System.out.println("I am from Fruit class F2 method");
+    }
+
+    public void number(){
+        System.out.println("My number = " +Mynumber);
+    }
+}
---
+++ SingleI.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+public class SingleI {
+    String Parentname;
+    int Parentage;
+    String Parentoccupation;
+
+    public void P1(){
+        System.out.println("I am a Teacher from parent class P1 method");
+    }
+
+    public void P2(){
+        System.out.println("I am a teacher from parent class P2 method");
+    }
+
+    
+
+}

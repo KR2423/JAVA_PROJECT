b8764b282783bc69dae38ca166e83883819eaa6e
KR2423 <ckeerthi.r23@gmail.com>
2025-07-31 14:48:25 -0700

    Day 5 - Single & Multilevel Inheritance
    Inheritance - Single Inheritance - Single, Child, Test1.java
    Multilevel Inheritance - using extends keyword

---
+++ Child.java
@@ -0,0 +1,17 @@
+package Inheritance;
+
+public class Child extends SingleI {
+    String childName;
+    String childClass;
+    int childAge;
+
+    public void C1(){
+        System.out.println("I am from child class C1 method");
+    }
+    public void C2(){
+        System.out.println("I am from Child class C2 method");
+    }
+
+
+    
+}
---
+++ Mango.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Mango extends Banana {
+    String F3name;
+    String F3color;
+    public void M1name(){
+        System.out.println("My name is Mango and I am 3rd child of Fruit");
+    }
+    public void M1color(){
+        System.out.println("My color is "+F3color);
+    }
+    
+}
